b7cf1146b557d3ec8c7025053fa5942977372f10
Germ-y <yunggi040919@naver.com>
2025-12-17 02:08:16 +0900

    feat: Set debug False

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yoursecretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["34.22.67.62",'localhost', '127.0.0.1']
 
