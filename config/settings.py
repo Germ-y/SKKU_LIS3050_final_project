945d4c94a8e238116790d00e28a968444f912d59
Germ-y <yunggi040919@naver.com>
2025-12-17 03:45:19 +0900

    feat: Changed allowed host (2->3)

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-yoursecretkey'
 DEBUG = False
 
 # "34.22.67.62"
-ALLOWED_HOSTS = ["24.64.103.195",'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["34.64.103.195",'localhost', '127.0.0.1']
 
 
 # Application definition
